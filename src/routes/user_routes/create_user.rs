70cf7afb6bf6ee1e9532a543baf259186e0102e9
Rahul721999 <rrmondal12@gmail.com>
2024-05-24 10:05:14 +0530

    feature: create-user API implemented

--- src/routes/user_routes/create_user.rs
+++ src/routes/user_routes/create_user.rs
@@ -1,5 +1,32 @@
-use actix_web::{HttpResponse, Responder};
+use crate::{
+    schema::{save_data, User},
+    AppState,
+};
+use actix_web::{http::StatusCode, web, HttpResponse, Responder};
+use log::{error, info};
+use serde::Deserialize;
 
-pub async fn create_user() -> impl Responder {
-    HttpResponse::Ok().body("create-user")
+#[derive(Deserialize)]
+pub struct NewUser {
+    name: String,
+}
+pub async fn create_user(
+    state_data: web::Data<AppState>,
+    req: web::Json<NewUser>,
+) -> impl Responder {
+    if let Ok(mut state_data) = state_data.data.lock() {
+        let new_user = User::new(&req.name);
+        let user_id = new_user.id;
+        
+        // add new user to the DB
+        state_data.users.insert(user_id, new_user);
+
+        // update the new data to DB
+        save_data(&state_data);
+
+        info!("User created successfully with ID: {}", user_id);
+        return HttpResponse::Ok().json(user_id);
+    };
+    error!("failed to get DB");
+    HttpResponse::InternalServerError().body("DB error")
 }
