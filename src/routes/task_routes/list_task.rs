d977b709100ce6e6866b3802ebbbd4871ec172fc
Rahul721999 <rrmondal12@gmail.com>
2024-05-25 00:08:23 +0530

    Feature: Added list-task API endpoint

--- src/routes/task_routes/list_task.rs
+++ src/routes/task_routes/list_task.rs
@@ -1,5 +1,40 @@
-use actix_web::{HttpResponse, Responder};
+use std::borrow::BorrowMut;
 
-pub async fn list_task() -> impl Responder{
-    HttpResponse::Ok().body("list-task")
+use crate::{
+    schema::{save_data, Status, Task, User},
+    AppState,
+};
+use actix_web::{http::StatusCode, web, HttpResponse, Responder};
+use chrono::NaiveDate;
+use log::{error, info, warn};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TaskList {
+    tasks: Vec<Task>,
+}
+
+/// API endpoint to create new task
+/// URL: "/users/{userid}/tasks/list"
+pub async fn list_task(
+    state_data: web::Data<AppState>,
+    user_id: web::Path<Uuid>,
+) -> impl Responder {
+    match state_data.data.lock() {
+        Ok(state_data) => {
+            let user_id = &user_id.into_inner();
+            if let Some(user) = state_data.users.get(user_id) {
+                let task_list: Vec<Task> = user.tasks.values().cloned().collect();
+                info!("Listing tasks for user ID: {}", user_id);
+                HttpResponse::Ok().json(TaskList { tasks: task_list })
+            } else {
+                HttpResponse::NotFound().body("User not found")
+            }
+        }
+        Err(_) => {
+            error!("Failed to acquire lock on the state data");
+            HttpResponse::InternalServerError().body("Internal Server Error")
+        }
+    }
 }
